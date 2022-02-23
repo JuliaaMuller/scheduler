f631d36dd547644af69af851e9b481db848673d8
JuliaaMuller <julialm.muller@gmail.com>
2022-02-23 15:49:53 -0500

    the schedule online

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,10 +8,8 @@ export default function Appointment(props) {
   const {time, interview} = props
   return (
     <article className="appointment">
-      {props.time ? <Header time = {time}/> : "No appointment"}
-      {props.interview ? <Show student ={interview.student} interviewers={interview.interviewer} /> : <Empty />}
+      {props.time ? <Header time={time}/> : "No appointment"}
+      {props.interview ? <Show student={interview.student} interviewers={interview.interviewer} /> : <Empty />}
       </article>
-    
-   
   )
 }
